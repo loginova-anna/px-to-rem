eb0fd21331ce6a28db86ce4e52af071bc020d3f7
Victoria <victoria@Vasilys-MacBook-Air.local>
2018-02-09 00:07:49 +0700

    working! replacing all matches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  input: string;
+  output: string;
+
+  ngOnInit() {
+
+  }
+
+  transform() {
+    if (!this.input) {
+      return;
+    }
+    var regex = /(\d+)\s*px/g;
+    this.output = this.input.replace(regex, this.pxtorem);
+  }
+
+  pxtorem(match) {
+    let px = parseInt(match);
+    return px/16 + 'rem';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
